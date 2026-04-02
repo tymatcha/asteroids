8d23b20aa320a745cbc4caa72c00ae930cdbf5e0
tymatcha <bigmandoingbusiness@gmail.com>
2026-04-02 17:32:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_obj = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock_obj.tick(60) / 1000
 
 
 if __name__ == "__main__":
