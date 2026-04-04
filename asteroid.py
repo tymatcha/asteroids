82e1de7b84d4fab92599a1fef76b1f8259eb1caa
tymatcha <bigmandoingbusiness@gmail.com>
2026-04-04 23:20:58 +0800

    ch4 l6 sol

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,23 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)  # nosec
+            new_asteroid1 = Asteroid(
+                self.position[0], self.position[1], self.radius / 2
+            )
+            new_asteroid2 = Asteroid(
+                self.position[0], self.position[1], self.radius / 2
+            )
+            new_asteroid1.velocity = (
+                pygame.math.Vector2.rotate(self.velocity, random_angle) * 1.2
+            )
+            new_asteroid2.velocity = (
+                pygame.math.Vector2.rotate(self.velocity, -random_angle) * 1.2
+            )
--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         log_state()
         screen.fill("black")
