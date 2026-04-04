7e33feec1bf0817ce260854f56677cdd813f33b1
tymatcha <bigmandoingbusiness@gmail.com>
2026-04-04 15:43:27 +0800

    ch4 l5 sol

--- main.py
+++ main.py
@@ -47,6 +47,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
 
         log_state()
         screen.fill("black")
